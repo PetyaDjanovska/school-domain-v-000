f243022cf175e467f16cbabcdcb0d53608944114
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-12 13:28:16 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,14 @@
 # code here!
 class School
+  attr_accessor :student_name, :grade
   attr_reader :roster
 
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def add_student (student_name, grade)
+
   end
 end
