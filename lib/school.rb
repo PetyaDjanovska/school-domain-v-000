86b6722b4ce3cfdf53c38d470c1369f0c254d54d
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-12 13:57:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,14 +3,12 @@ class School
   attr_accessor :student_name, :grade
   attr_reader :roster
 
-  @rooster[:grade] = []
-
   def initialize(name)
     @name = name
     @roster = {}
   end
 
   def add_student (student_name, grade)
-    @roster[:grade] << student_name
+    @roster[grade]
   end
 end
