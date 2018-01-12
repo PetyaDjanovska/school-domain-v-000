ce8f586175074b5e0ffdb09645c42f318bd70269
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-12 15:32:40 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 class School
-  attr_accessor :student_name, :grade, :students
+  attr_accessor :student_name, :grade
   attr_reader :roster
 
   def initialize(name)
@@ -11,8 +11,7 @@ class School
   end
 
   def add_student (student_name, grade)
-    @roster[grade] = [students]
-    students << student_name
+    
   end
 
 
